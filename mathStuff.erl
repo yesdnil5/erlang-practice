f17d0ceacde864417851b140d7306e3d1526d7fb
yesdnil5 <yesdnil5@gmail.com>
2013-12-22 16:35:17 -0500

    adding the math stuffs

---
+++ mathStuff.erl
@@ -0,0 +1,15 @@
+-module(mathStuff).
+-export([factorial/1, area/1]).
+
+factorial(0) -> 1;
+factorial(N) -> N * factorial(N-1).
+
+area({square, Side}) ->
+	Side * Side;
+area({circle, Radius}) ->
+	3 * Radius * Radius;
+area({triangle, A, B, C}) ->
+	S = (A+B+C)/2,
+	math:sqrt(S*(S-A)*(S-B)*(S-C));
+area(Other) ->
+	{invalid_object, Other}.
